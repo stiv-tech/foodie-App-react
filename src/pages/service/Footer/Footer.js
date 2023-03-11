3f82be070880565052a58d2af16481b1f7fc98e6
STEPHENCHISKY <stephenchisky@yahoo.com>
2023-03-11 01:25:47 +0100

    reEdited

--- src/pages/service/Footer/Footer.js
+++ src/pages/service/Footer/Footer.js
@@ -7,10 +7,17 @@ function Footer() {
        <div className='footer-pg'>
          <h2 className='footer-item'>Download the app now.</h2>
          <p className='footer-para'>Available on your favorite store. Start your premium experience now</p>
+         <p className='footer-para1'>Most calendars are <br/> designed for teams. </p>
 
          <div className='footer-btn'>
             <button className='foot-btn1'>Playstore</button>
             <button className='foot-btn11'>App store</button>
+            
+         </div>
+         <div className='footer-btnn'>
+            <button className='foot-btn1'>Buy now</button>
+            <button className='foot-btn11'>Try for free</button>
+            
          </div>
        </div> 
       
